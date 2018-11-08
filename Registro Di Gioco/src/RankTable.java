d8895026cf5f1cb2ce643e55c7568be512d3e9fe
Unknown <marco.pontone@gmail.com>
2018-11-08 23:20:50 +0100

    Report 08/11/2018
    Fix nella classe DbManager che impediva l'accesso al database. Fix della funzione caricaPlayerList nella classe DbManager, che impediva il corretto caricamento nella tabella RankTable. Completata la funzione inviaItem nella classe DbManage, aggiunta dell’EventHandler nella classe principale e l’aggiunta del metodo setOnAction() al bottone CreateItem, entrambi correttamente funzionanti per l’invio e l’INSERT nel database di un oggetto Item. Aggiunta di clear() dopo l'invio corretto di Item al db.

--- Registro Di Gioco/src/RankTable.java
+++ Registro Di Gioco/src/RankTable.java
@@ -1,42 +1,47 @@
 import javafx.collections.*;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.*;
+import java.util.*;
 
 public class RankTable extends TableView<Player> {
     private ObservableList<Player> list;
+    private DbManager db;
+    private ConfigurazioneXML configurazione;
     
     public RankTable(){
-    
         
         TableColumn nameColumn = new TableColumn("Username");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         nameColumn.setMinWidth(120);
         
         TableColumn factionColumn = new TableColumn("Faction");
-        factionColumn.setCellFactory(new PropertyValueFactory<>("faction"));
+        factionColumn.setCellValueFactory(new PropertyValueFactory<>("faction"));
         factionColumn.setMinWidth(120);
         
         TableColumn classColumn = new TableColumn("Class");
-        classColumn.setCellFactory(new PropertyValueFactory<>("class"));
+        classColumn.setCellValueFactory(new PropertyValueFactory<>("classe"));
         classColumn.setMinWidth(120);
         
         TableColumn levelColumn = new TableColumn("Level");
-        levelColumn.setCellFactory(new PropertyValueFactory<>("level"));
+        levelColumn.setCellValueFactory(new PropertyValueFactory<>("level"));
         levelColumn.setMinWidth(120);
         
         TableColumn ratingColumn = new TableColumn("Rating");
-        ratingColumn.setCellFactory(new PropertyValueFactory<>("rating"));
+        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
         ratingColumn.setMinWidth(120);
         
-        list = FXCollections.observableArrayList();
         setItems(list);
+        
         getColumns().addAll(nameColumn, factionColumn, classColumn, levelColumn, ratingColumn);
         
         lookup(".scoll-bar:vertical");
+        
+        this.setLayoutX(550);
+        this.setLayoutY(40);
     }
     
-    public void updateTableList(ObservableList<Player> newList){
-        list.clear();
-        list.addAll(newList);
+    public void updateTableList(List<Player> newList){
+       ObservableList<Player> list = FXCollections.observableArrayList (newList);
+        setItems(list);
     }
 }
